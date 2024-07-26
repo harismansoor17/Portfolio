0827e879ebb902f3b5508d2e9c05480e9f019b04
Haris Mansoor <152084312+harismansoor17@users.noreply.github.com>
2024-07-27 01:07:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ var typed = new Typed("#element", {
   typeSpeed: 25,
 });
          //  
+        //  SMTP JS
+submitBtn. addEventListener("click", () => {
+let email = document.querySelector("#email");
+let subject = document.querySelector("#subject");
+let name = document.querySelector("#name");
+let message = document.querySelector("#message");
+
+  Email.send({
+    SecureToken: "02466251-0830-4d33-9541-8d1c49342502",
+    To : 'harisdev2005@gmail.com',
+    From : email.value,
+    Subject : subject.value,
+    Body : "Name: " + name.value 
+          + "<br> Email: " + email.value
+          + "<br> Message: " + message.value
+  }).then(
+  message => alert(message)
+  ).then(
+    message => alert("Submited")
+  )
+
+});
+
+
+            // 
