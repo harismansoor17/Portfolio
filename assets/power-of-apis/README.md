e0ca2ec6d1b1b5efebfe3fb549168311d8adbce1
harismansoor17 <mansoorfarooqui7049@gmail.com>
2024-10-21 16:23:42 +0500

    projects added

---
+++ assets/power-of-apis/README.md
@@ -0,0 +1,41 @@
+## Power Of APIs
+
+<h2>Introduction</h2>
+<p>Power of APIs is a personal project made to show the capability of APIs,</p>
+<p>This Project has also help me in improving my understanding about APIs and how to use them efficiently.</p>
+<br>
+<br>
+<h2>How to View and Use</h2>
+<p>This Power of APIs is designed to be viewed as a static website. Simply open the index.html file in your browser to explore the content or Go to Live Demo: (https://harismansoor17.github.io/Power-of-APIs/)</p>
+
+
+<h2>How it works</h2>
+<p>Power of APIs is a collection of App which fetch data from Publicaly available APIs,</p>
+<span>It includes: </span>
+<ol>
+<li>Weather API (Open Weather Map API)</li>
+<li>Currency Converter (Exchange Rate-Api)</li>
+<li>Joke Generator (Joke API)</li>
+<li>Cat Image Generator (The Cat API)</li>
+</ol>
+<br>
+<br>
+<h2>TailWind CSS</h2>
+<p>Power of APIs uses TailWind CSS instead of vanilla CSS because TailWind saves alot of development time as otherwise Vanilla Css would have been 1000+ lines of code.</p>
+<br>
+<br>
+<h2>Internal JavacScript</h2>
+<p>Power of APIs uses Internal JS inside of every indidvidual HTML file for simplicity and to make debuging Easier </p>
+<br>
+<h3>Currency Converter</h3>
+<p>Only app which uses vanilla Css,Because I learned the value of Tailwind Css The Hard Way.</p>
+
+
+
+
+
+
+
+
+
+
---
+++ assets/zirax/script.js
@@ -0,0 +1,95 @@
+// Cart
+const cartNav = document.querySelector("#cartNav");
+
+function showCart() {
+  cartNav.style.right = "0";
+}
+
+function hideCart() {
+  cartNav.style.right = "-400px";
+}
+
+let cart = []; 
+
+function addToCart(productId) {
+  const product = products.find((item) => item.id === productId);
+  if (!product) {
+    return; 
+  }
+
+  
+  const existingProduct = cart.find((item) => item.id === productId);
+  
+  if (existingProduct) {
+    
+    existingProduct.quantity++;
+    document.getElementById(`quantity-${productId}`).value = existingProduct.quantity;
+  } else {
+    
+    cart.push({ ...product, quantity: 1 });
+
+    
+    const { image, title, price } = product;
+    document.getElementById("cartItems").innerHTML += `
+      <div class="w-full text-black bg-white border-blue-700 border-b-2 border-l-2 relative flex justify-evenly items-center" id="cart-item-${productId}">
+        <button
+          onclick="removeFromCart(${productId})"
+          class="absolute top-0 right-0 m-2 text-xl hover:scale-110 transition-all duration-300 delay-150"
+        >
+          <i class="fa-regular fa-rectangle-xmark"></i>
+        </button>
+        <div class="flex justify-evenly w-full">
+        <div class="w-24 rounded-xl">
+        <img style="height:100px; width:100px; border-radius:12px; margin:1rem;" src="${image}" alt="${productId} Product Image" />
+        </div>
+        <div class=" flex flex-col justify-center">
+        <span class="text-md font-bold">${title}</span>
+        <input style="color:white; background-color:#1d4ed8; font-size:12px; outline:none; border-radius:12px; width:50px; padding:4px;" sty id="quantity-${productId}" type="number" value="1" min="1" onchange="updateQuantity(${productId}, this.value)">
+        <span class="text-xl font-semibold">price: <span class="price">${price}$</span></span>
+        </div>
+        </div>
+      </div>`;
+  }
+
+  updateCartTotal();
+  updateItemsInCart(); 
+}
+
+function removeFromCart(productId) {
+  
+  cart = cart.filter((item) => item.id !== productId);
+
+  
+  const cartItemElement = document.getElementById(`cart-item-${productId}`);
+  if (cartItemElement) {
+    cartItemElement.remove();
+  }
+
+  updateCartTotal();
+  updateItemsInCart();
+}
+
+function updateQuantity(productId, newQuantity) {
+  const product = cart.find((item) => item.id === productId);
+  if (product) {
+    product.quantity = parseInt(newQuantity, 10);
+  }
+
+  updateCartTotal();
+}
+function updateCartTotal() {
+  const cartTotal = document.getElementById("cartTotal");
+  let total = 0;
+  cart.forEach((product) => {
+    total += product.price * product.quantity;
+  });
+
+  cartTotal.textContent = `$${total.toFixed(2)}`;
+}
+
+
+function updateItemsInCart() {
+  const itemsInCart = document.getElementById('itemsInCart');
+
+  itemsInCart.textContent = cart.length;
+}
